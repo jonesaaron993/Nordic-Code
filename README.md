62e114a4b61993c14736f8c465f6a67e9caf5ce7
Aaron Paul Jones <jonesaaron993@gmail.com>
2020-06-23 21:28:24 -0500

    Added ability to create writting to terminal statment. Also updated some code from the last big commit. Plan to fix a couple of items in the code later on, but also plan on introducing data types soon.

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ This repo is used to create my own programming language called Nordic Code.
 
 **THIS IS A WORK IN PROGRESS PROJECT**
 
-Currently working on the interpreter to read in the helloworld.nord test file and parse through each line to understand and interpret what to do with that line.
+~~Currently working on the interpreter to read in the helloworld.nord test file and parse through each line to understand and interpret what to do with that line.~~
+
+Planning to work with different data types next, mostly with integers and strings for right now.
--- interpreter.c
+++ interpreter.c
@@ -1,14 +1,13 @@
 /* This is a very, very, very, basic interpreter for the nordic code base. 
    Work to expand this code base is in the works, but it will be a while
    before this interpreter is fully operational.
-   ALL CODE HERE AT THIS CURRENT WRITING DATE IS NOT PERFECT, AND CAN
-   BE IMPROVED. */
+   ALL CODE HERE AT THIS CURRENT DATE IS NOT PERFECT, AND CAN BE IMPROVED. */
 
 #include <stdio.h>
-#define WINDOWS
 #include <direct.h>
 #include <string.h>
 #include <assert.h>
+#include<stdbool.h>
 
 //Function prototypes
 
@@ -17,19 +16,29 @@ const char* getTestDir(char currentDir[FILENAME_MAX]);
 
 //Open a file and print all of its contents
 void openAndPrintFile(char currentDir[FILENAME_MAX]);
- 
-int main(){
+
+//Check if a statment starts with certain values
+bool StartsWith(const char *a, const char *b);
+
+//Print out the paramaters from the nord print statment
+void printFromQuotes(char *input);
+
+int main()
+{
    //Get the current directory
    char currentDir[FILENAME_MAX];
 
    //Add the tests folder to the char array
    getTestDir(currentDir);
 
+   //Get the number of lines
    openAndPrintFile(currentDir);
 
   return 0;
 }
 
+/*Navigate to the test directory 
+and load in the file*/
 const char* getTestDir(char currentDir[FILENAME_MAX])
 {
    //Get the current directory
@@ -38,7 +47,12 @@ const char* getTestDir(char currentDir[FILENAME_MAX])
    //The path to the test nordcode file
    char tests[] = "/tests/helloworld.nord";
 
-   //TODO: Make sure the file is a .nord file
+   //Make sure that the file is a valid file
+   //by checking its extention
+   char * ext = strrchr(tests, '.');
+
+   //Assert if the extention is not a nord file
+   assert(ext != "nord");
 
    //Concatenate the strings
    strcat(currentDir, tests);
@@ -46,20 +60,55 @@ const char* getTestDir(char currentDir[FILENAME_MAX])
    return currentDir;
 }
 
+/*Open all contents of the file
+and split them into multilines*/
 void openAndPrintFile(char currentDir[FILENAME_MAX])
 {
    //Open the file with the current path
    FILE *testFile = fopen(currentDir, "r");
 
+   //TODO: Make this a dynamic array
    char currentLine[100];
 
    //Assert that the file location is not empty
    assert(testFile != NULL);
 
+   //TODO: Improve this while loop for counting number of lines
    while (fgets(currentLine, sizeof(currentLine), testFile) != NULL)
    {
-      fprintf(stderr, currentLine);
+      //Check if the statment is a print statment
+      if (StartsWith(currentLine, ".."))
+      {
+         char *array = currentLine;
+         printFromQuotes(array);
+      }
    }
 
+   //Close the file
    fclose(testFile);
+}
+
+/*Check the begining values of the lines
+if a line starts with '..' that means
+the user is trying to output to the terminal*/
+bool StartsWith(const char *a, const char *b)
+{
+   if(strncmp(a, b, strlen(b)) == 0) return 1;
+   return 0;
+}
+
+/*Print the contents between quotes
+of the nord print statment*/
+void printFromQuotes(char *input)
+{
+   char a[50];
+   
+   if(sscanf(input,"%*[^']'%[^']'",a)==1)
+   {
+      printf("%s\n", a);
+   } 
+   else
+   {
+      printf("%s\n", a);
+   }
 }
